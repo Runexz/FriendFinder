045559eea4aea440ffb32831ae7c9490817b4f81
Ricardo Rodriguez <m3ssiak@gmail.com>
2019-09-08 16:32:24 -0500

    var friends created with dummy entry

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,1 +1,22 @@
 // friends.js file
+
+//DATA
+//initial entry dummy friend, will hold all friends in JSON
+var friends = [
+    {
+        name: "Strider",
+        photo: "https://vignette.wikia.nocookie.net/vsbattles/images/1/12/Strider.png/revision/latest?cb=20151113063020",
+        scores: [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    }
+];
