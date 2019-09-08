deb513ce111902cb6e1689a684891b80e9c27945
Ricardo Rodriguez <m3ssiak@gmail.com>
2019-09-08 16:48:26 -0500

    setup express to handle routes and start server

--- server.js
+++ server.js
@@ -1,5 +1,23 @@
 // server.js
 
+//NPM express package
 var express = require("express");
 
-var path = require("path");
+// node is notified that an "express" server is created
+var app = express();
+
+// sets an initial port heroku will use its own port or localhost:8080 will be used
+var PORT = process.env.PORT || 8080;
+
+// express app will handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// server is pointed to these 2 rout files if the user visits or requests data from a URL
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+// initiates server
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
