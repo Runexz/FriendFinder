611702d4e4060e3f2c7d7d1e445a1705f7ef4dc9
Ricardo Rodriguez <m3ssiak@gmail.com>
2019-09-08 16:16:37 -0500

    require path and sendfile path tosurvey/home added

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,1 +1,17 @@
 // htmlRoutes.js
+
+// node path package needed to get the correct file path for our html
+var path = require("path");
+
+module.exports = function (app) {
+
+    //if /survey is entered then survey.html is shown
+    app.get("/survey", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
+    });
+
+    // If no matching route is found default to home
+    app.get("*", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
+}
