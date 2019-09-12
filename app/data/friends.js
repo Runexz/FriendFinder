b87be5afe20abb01b2e833cda56b94d2053fb195
Ricardo Rodriguez <m3ssiak@gmail.com>
2019-09-11 20:48:18 -0500

    hardcoded more friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -19,6 +19,54 @@ var friends = [
             4,
             1
         ]
+    },
+    {
+        name: "Sonic",
+        photo: "http://info.sonicretro.org/images/7/71/Unleashed_sonic.jpg",
+        scores: [
+            4,
+            3,
+            3,
+            3,
+            3,
+            1,
+            1,
+            3,
+            3,
+            5
+        ]
+    },
+    {
+        name: "Mario",
+        photo: "http://pluspng.com/img-png/super-mario-png-super-mario-bros-new-super-mario-bros-super-mario-galaxy-mario-900.jpg",
+        scores: [
+            1,
+            1,
+            3,
+            2,
+            5,
+            4,
+            2,
+            5,
+            3,
+            3
+        ]
+    },
+    {
+        name: "Link",
+        photo: "http://www.smashbros.com/images/og/link.jpg",
+        scores: [
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3
+        ]
     }
 ];
 
