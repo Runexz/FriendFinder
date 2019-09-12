4b3fe55beca3afcfb483da0ee2d8705a804185c4
Ricardo Rodriguez <m3ssiak@gmail.com>
2019-09-11 19:02:56 -0500

    created second loop

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -14,11 +14,36 @@ module.exports = function (app) {
     app.post("/api/friends", function (req, res) {
 
         // Function stuff here
+        // create variable that will fill in with best match
+        var topMatch = {
+            name: "",
+            img: "",
+            difference: 20
+        };
+
+        //create variables to link to server api
+        var newFriends = req.body;
+        var newName = req.body.newName;
+        var newImg = req.body.newImg;
+        var newScores = req.body.scores;
+        var startDif = 0;
+
 
         //for loop needed to loop through all the friends
         for (var i = 0; i < friends.length; i++) {
             var totalFriends = friends[i];
-            
+            startDif = 0;
+
+            // second loop needed to compare new friend score to the friend data
+            for (var j = 0; j < totalFriends.scores.length; j++) {
+                var totalFriends2 = totalFriends.scores[j];
+
+                //math.abs is the absolute difference between friend and new friend and adds to startDif
+                startDif += Math.abs(totalFriends2 - newScores[j]);
+
+                
+                
+            }
         }
 
         friends.push(req.body);
